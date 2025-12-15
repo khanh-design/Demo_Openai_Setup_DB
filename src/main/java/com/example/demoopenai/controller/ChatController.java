ca0d9f11b427b7b28a59a631e39fc18659913edd
khanh-design <buiduykhanh1062003@gmail.com>
2025-12-15 17:16:22 +0700

    up

--- src/main/java/com/example/demoopenai/controller/ChatController.java
+++ src/main/java/com/example/demoopenai/controller/ChatController.java
@@ -4,7 +4,6 @@ import com.example.demoopenai.dto.ChatRequest;
 import com.example.demoopenai.service.ChatService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
