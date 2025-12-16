c8c05f8c9ee1098902dc158d862f0324b1f8be30
khanh-design <buiduykhanh1062003@gmail.com>
2025-12-17 00:00:54 +0700

    create image processing successfull

--- src/main/java/com/example/demoopenai/controller/ChatController.java
+++ src/main/java/com/example/demoopenai/controller/ChatController.java
@@ -4,7 +4,9 @@ import com.example.demoopenai.dto.ChatRequest;
 import com.example.demoopenai.service.ChatService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ChatController {
@@ -17,5 +19,11 @@ public class ChatController {
     @PostMapping("/chat-message")
     public String sendMessage(@RequestBody ChatRequest chatRequest) {
         return chatService.generate(chatRequest);
+    }
+
+    @PostMapping("/chat-with-image")
+    public String chatWithImage(@RequestParam("file") MultipartFile file,
+                                @RequestParam("message") String message) {
+        return chatService.chatWithImage(file, message);
     }
 }
--- src/main/java/com/example/demoopenai/service/ChatService.java
+++ src/main/java/com/example/demoopenai/service/ChatService.java
@@ -4,9 +4,14 @@ import com.example.demoopenai.dto.ChatRequest;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.content.Media;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ChatService {
@@ -25,6 +30,27 @@ public class ChatService {
 
         return chatClient
                 .prompt(prompt)
+                .call()
+                .content();
+    }
+
+    public String chatWithImage(MultipartFile file, String message) {
+        Media media = Media.builder()
+                .mimeType(MimeTypeUtils.parseMimeType(file.getContentType()))
+                .data(file.getResource())
+                .build();
+
+//        AI sẽ tự thảo sức sáng tạo nội dung
+        ChatOptions chatOptions = ChatOptions.builder()
+                .temperature(1D)
+                .build();
+
+        return chatClient.prompt()
+                .options(chatOptions)
+                .system("you are Derive.AI")
+                .user(promptUserSpec
+                        -> promptUserSpec.media(media)
+                        .text(message))
                 .call()
                 .content();
     }
